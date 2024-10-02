64c35d286f2994b81e7cdca6265c9ade77953a8f
ItzRikk <119906679+ItzRikk@users.noreply.github.com>
2024-10-02 15:52:09 -0700

    Finish LogIn Page

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './LoginForm.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 const LoginForm = () =>{
     return (
@@ -8,9 +10,11 @@ const LoginForm = () =>{
             <h1>Login</h1>
             <div className="input-box">
                 <input type="text" placeholder='Username' required />
+                <FaUser className="icon" />
             </div>
             <div className="input-box" >
                 <input type="password" placeholder='Password' required />
+                <FaLock className="icon"/>
             </div>
             < button type="submit">Login</button>
             <div className="register-link" >
